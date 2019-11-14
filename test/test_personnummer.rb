fce3b6bfbbd5a067d3d0376ed0ba2dde357f3a25
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-11-14 21:44:16 +0100

    Update test cases

--- test/test_personnummer.rb
+++ test/test_personnummer.rb
@@ -3,21 +3,17 @@ require 'personnummer'
 
 class PersonnummerTest < Minitest::Test
     def test_with_control_digit
-        assert_equal true, Personnummer::valid(6403273813)
-        assert_equal true, Personnummer::valid('510818-9167')
-        assert_equal true, Personnummer::valid('19900101-0017')
-        assert_equal true, Personnummer::valid('19130401+2931')
-        assert_equal true, Personnummer::valid('196408233234')
-        assert_equal true, Personnummer::valid('000101-0107')
-        assert_equal true, Personnummer::valid('0001010107')
-        assert_equal true, Personnummer::valid('200002296127')
+        assert_equal true, Personnummer::valid(8507099805)
+        assert_equal true, Personnummer::valid('198507099805')
+        assert_equal true, Personnummer::valid('198507099813')
+        assert_equal true, Personnummer::valid('850709-9813')
+        assert_equal true, Personnummer::valid('196411139808')
     end
 
     def test_without_control_digit
-        assert_equal false, Personnummer::valid(640327381)
-        assert_equal false, Personnummer::valid('510818-916')
-        assert_equal false, Personnummer::valid('19900101-001')
-        assert_equal false, Personnummer::valid('100101+001')
+        assert_equal false, Personnummer::valid('19850709980')
+        assert_equal false, Personnummer::valid('19850709981')
+        assert_equal false, Personnummer::valid('19641113980')
     end
 
     def test_wrong_personnummer_or_types
@@ -26,16 +22,12 @@ class PersonnummerTest < Minitest::Test
         assert_equal false, Personnummer::valid({})
         assert_equal false, Personnummer::valid(false)
         assert_equal false, Personnummer::valid(true)
-        assert_equal false, Personnummer::valid(1122334455)
+        assert_equal false, Personnummer::valid(0)
         assert_equal false, Personnummer::valid('112233-4455')
         assert_equal false, Personnummer::valid('19112233-4455')
+        assert_equal false, Personnummer::valid('20112233-4455')
         assert_equal false, Personnummer::valid('9999999999')
         assert_equal false, Personnummer::valid('199999999999')
-        assert_equal false, Personnummer::valid('9913131315')
-        assert_equal false, Personnummer::valid('9911311232')
-        assert_equal false, Personnummer::valid('9902291237')
-        assert_equal false, Personnummer::valid('19990919_3766')
-        assert_equal false, Personnummer::valid('990919_3766')
         assert_equal false, Personnummer::valid('199909193776')
         assert_equal false, Personnummer::valid('Just a string')
         assert_equal false, Personnummer::valid('990919+3776')
@@ -44,8 +36,10 @@ class PersonnummerTest < Minitest::Test
     end
 
     def test_coordination_numbers
-        assert_equal true, Personnummer::valid('701063-2391')
-        assert_equal true, Personnummer::valid('640883-3231')
+        assert_equal true, Personnummer::valid('198507699802')
+        assert_equal true, Personnummer::valid('850769-9802')
+        assert_equal true, Personnummer::valid('198507699810')
+        assert_equal true, Personnummer::valid('850769-9810')
     end
 
     def test_wrong_coordination_numbers
